b770a2e0b4bec23985c35efa6ad9db5e5fa85439
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-10 00:10:03 -0500

    Mongo Rating model schema

---
+++ src/rating/entities/rating.entity.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { Place } from '../../place/entities/place.entity';
+
+@Schema()
+export class Rating extends Document {
+  @Prop({
+    required: true,
+    min: 1,
+    max: 5,
+  })
+  rate: number;
+
+  @Prop()
+  comments: string;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Place',
+  })
+  place: Place;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  })
+  user: string;
+}
+
+export const RatingSchema = SchemaFactory.createForClass(Rating)
+  .set('versionKey', false)
+  .set('timestamps', true);
