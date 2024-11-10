2bee80daf42a55ffaa4d78239370f0e01d6d976d
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-09 19:35:42 -0500

    Add MongooseModule config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
+import { envs } from './config';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    MongooseModule.forRoot(envs.mongoUrl),
   ],
 })
 export class AppModule {}
