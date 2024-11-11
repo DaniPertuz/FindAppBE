69f340288daa30f41495e33975a57ccdd34cef2f
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-10 21:23:43 -0500

    Mongo Product model schema

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,54 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { Place } from '../../place/entities/place.entity';
+
+@Schema()
+export class Product extends Document {
+  @Prop({
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    required: false,
+  })
+  description: string;
+
+  @Prop([String])
+  category: string[];
+
+  @Prop({
+    required: false,
+  })
+  observation?: string;
+
+  @Prop({
+    required: true,
+  })
+  currency: string;
+
+  @Prop({
+    required: true,
+  })
+  price: number;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Place',
+  })
+  place: Place;
+
+  @Prop({
+    default: 0,
+  })
+  rate: number;
+
+  @Prop({
+    required: false,
+  })
+  img?: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product)
+  .set('versionKey', false)
+  .set('timestamps', true);
