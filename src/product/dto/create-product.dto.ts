e7969ca9fbeca4799ee4168106ec90c5b3277784
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-10 21:25:34 -0500

    Product DTOs

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsArray,
+  IsMongoId,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUrl,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @MaxLength(30)
+  name: string;
+
+  @IsString()
+  @MaxLength(200)
+  description: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  category: string[];
+
+  @IsString()
+  currency: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsMongoId()
+  place: string;
+
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  img?: string;
+}
