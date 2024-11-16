64d0ebef419386a10b09697aa1c5931cc18f9823
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-16 15:11:13 -0500

    Add necessary modules to import, export, and provide JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { User, UserSchema } from '../user/entities/user.entity';
 import { UserModule } from '../user/user.module';
+import { envs } from '../config';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
     MongooseModule.forFeature([
       {
@@ -16,6 +20,16 @@ import { UserModule } from '../user/user.module';
       },
     ]),
     UserModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+    }),
+    JwtModule.register({
+      secret: envs.secretKey,
+      // signOptions: {
+      //   expiresIn: '8h',
+      // },
+    }),
   ],
+  exports: [PassportModule, JwtModule],
 })
 export class AuthModule {}
