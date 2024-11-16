df7d2f96a5245ed1c624e56e99e1ba37d9ffc0bb
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2024-11-16 15:02:17 -0500

    Add private key environment variable

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,12 +4,14 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: string;
   MONGO_URL: string;
+  SECRETORPRIVATEKEY: string;
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
     MONGO_URL: joi.string().required(),
+    SECRETORPRIVATEKEY: joi.string().required(),
   })
   .unknown(true);
 
@@ -22,4 +24,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   mongoUrl: envVars.MONGO_URL,
+  secretKey: envVars.SECRETORPRIVATEKEY,
 };
