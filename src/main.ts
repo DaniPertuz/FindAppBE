0d530bcfa953b785065eb85a479bb9c4a39c708b
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-29 00:09:29 -0500

    Add rawBody middleware to handle special requests' body

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
+import { rawBodyMiddleware } from './payments/middleware/raw-body.middleware';
 
 async function bootstrap() {
   const PORT = envs.port;
   const logger = new Logger('FindApp');
   const app = await NestFactory.create(AppModule);
+
+  app.use(rawBodyMiddleware);
 
   app.setGlobalPrefix('api');
 
