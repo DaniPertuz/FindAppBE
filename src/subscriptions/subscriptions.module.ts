795ae21e9ec5baf144e9de39f79f7d21209930d4
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-30 15:41:10 -0500

    Add Mailer and Place modules

--- src/subscriptions/subscriptions.module.ts
+++ src/subscriptions/subscriptions.module.ts
@@ -6,12 +6,16 @@ import {
   Subscription,
   SubscriptionSchema,
 } from './entities/subscription.entity';
+import { MailerModule } from '../mailer/mailer.module';
+import { PlaceModule } from '../place/place.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Subscription.name, schema: SubscriptionSchema },
     ]),
+    MailerModule,
+    PlaceModule,
   ],
   controllers: [SubscriptionsController],
   providers: [SubscriptionsService],
