a606988b47c5fc7dae13346248895f9db7d31b9c
Daniel Pertuz <ddpertuzoroz@hotmail.com>
2025-06-30 15:40:48 -0500

    Implement emails service when subscription is created

--- src/subscriptions/subscriptions.service.ts
+++ src/subscriptions/subscriptions.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { MailerService } from '../mailer/mailer.service';
+import { PlaceService } from '../place/place.service';
 import { Subscription } from './entities/subscription.entity';
+import { getPremiumHtml } from '../utils/mailerHtml';
 
 @Injectable()
 export class SubscriptionsService {
   constructor(
     @InjectModel(Subscription.name)
     private readonly subscriptionModel: Model<Subscription>,
+    private readonly mailerService: MailerService,
+    private readonly placeService: PlaceService,
   ) {}
 
   async create(data: Partial<Subscription>) {
-    return this.subscriptionModel.create(data);
+    const subscription = await this.subscriptionModel.create(data);
+
+    const place = await this.placeService.findOne(data.placeId);
+    if (place.email) {
+      const html = getPremiumHtml(data.premium, place.email);
+      await this.mailerService.sendMail(place.email, html, data.premium);
+    }
+
+    return subscription;
   }
 }
